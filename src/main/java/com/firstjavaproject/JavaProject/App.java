3543815ad1180fe8caf0988c882b1dd8c029de29
siddharthdiddi <siddharthdiddi@gmail.com>
2024-11-23 15:10:32 +0530

    first commit

---
+++ src/main/java/com/firstjavaproject/JavaProject/App.java
@@ -0,0 +1,13 @@
+package com.firstjavaproject.JavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
